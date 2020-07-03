fbda9479e8c4cc84df2fa53ff03ada107c434321
takaonishino1977 <62580946+takaonishino1977@users.noreply.github.com>
2020-07-03 10:41:50 +0900

    firestoreをmodelから呼んで表示 mapの勉強

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => BookList()),
+                          MaterialPageRoute(builder: (context) => BookListPage()),
                         );
                       },
                     )
